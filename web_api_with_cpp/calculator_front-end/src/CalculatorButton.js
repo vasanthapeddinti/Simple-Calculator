f5b78c5d31d95e7b183342563de0272205d66806
Vasantha Peddinti <v.peddinti@f5.com>
2024-01-06 05:59:57 +0000

    feat: calculator front end

---
+++ web_api_with_cpp/calculator_front-end/src/CalculatorButton.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function CalculatorButton(props) {
+    
+    return(
+        <input 
+        type='button'
+        value={props.caption} 
+        className={props.caption === "=" ? "calculator-equal-button" : null}
+        onClick={props.onClick}/>
+    );
+}
+
+export default CalculatorButton;
---
+++ web_api_with_cpp/calculator_front-end/src/CalculatorDisplay.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function CalculatorDisplay(props) {
+    
+    return (
+        <input 
+            value={props.text}
+            readOnly={true}
+            className="calculator-display" />
+    )
+}
+
+export default CalculatorDisplay;
